b69183c5dcafce2c8424aa4db3ed5be0bc358f20
v <naeini.v@gmail.com>
2019-12-20 16:01:56 +0330

    modify types

--- read_swagger_file.py
+++ read_swagger_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3.7
 import yaml
 import urllib.request
+from typing import Dict, Any
+from .types import Operation, RestMethod
 
-def read_yaml(url: str) -> dict:
+def read_yaml(url: str) -> Dict[str, Operation]:
     """
     >>> url = "https://raw.githubusercontent.com/swagger-api/swagger-samples/master/java/inflector-dropwizard-guice/src/main/swagger/swagger.yaml"
     >>> api_schema = read_yaml(url)
@@ -19,6 +21,24 @@ def read_yaml(url: str) -> dict:
     text = request.read()
     return yaml.load(text)
 
+def operation_from_path_info(api_schema: Dict[str, Any], path: str, method: RestMethod) -> Operation:
+    """
+    >>> api_schema = read_yaml(url)
+    >>> path = "/pet"
+    >>> method = "post"
+    >>> operation = operation_from_path_info(api_schema, path, RestMethod(method))
+    """
+    p = api_schema['paths'][path][f"{method}""]
+    out = Operation(
+        operations_id = p['OperationId'], 
+        path = path,
+        method = method, 
+        consumes = p['consumes'],
+        produces = p["produces"],
+        parameters = p["parameters"],   # it's a bit nested gonna use better type hints and so on 
+        responses = p["responses"],
+        security = p["security"]
+    )
 
 if __name__ == "__main__":
     import doctest
--- types.py
+++ types.py
@@ -1,7 +1,43 @@
+from enum import Enum, auto
 from typing import NamedTuple, Any, Dict
+import enum
+
+
+class StrOptions(str, Enum):
+    def __str__(self):
+        return self.value.lower()
+    
+    def _generate_next_value_(name, *_):
+        return name
+
+    @classmethod
+    def from_value(cls, value):
+        return cls._value2member_map_[value]
+
+class RestMethod(StrOptions):
+    GET = auto()
+    POST = auto()
+    PUT = auto()
+    DELETE = auto()
+    CONNECT = auto()
+    OPTIONS = auto()
+    TRACE = auto()
+    PATCH = auto()
+    
 
 class HttpResult(NamedTuple):
     Status: int
     Body: Any
     Headers: Dict[str, str]
-    
+
+
+class Operation(NamedTuple):
+    operations_id: str
+    path: str
+    method: RestMethod
+    consumes: Any
+    produces: Any
+    parameters: Any  # it's a bit nested gonna use better type hints and so on 
+    responses: Any
+    security: Any
+
